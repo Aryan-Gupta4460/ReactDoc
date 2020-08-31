904cd7922d9c64c7a83fc5010c267d4f4520e486
Aryan Gupta <62221758+Aryan-Gupta4460@users.noreply.github.com>
2020-09-01 00:06:34 +0530

    Add files via upload

---
+++ build/precache-manifest.1fa7e666ef7b28c2d827f72d466f9663.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c431e26329a6376d6ca3a406e78b7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68f31ea43418eb338ab9",
+    "url": "/static/css/main.c322a29a.chunk.css"
+  },
+  {
+    "revision": "bae59cac21b23814ebd5",
+    "url": "/static/js/2.305a99fd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.305a99fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68f31ea43418eb338ab9",
+    "url": "/static/js/main.e1b90ca6.chunk.js"
+  },
+  {
+    "revision": "89497b5c0fcff50bc045",
+    "url": "/static/js/runtime-main.f00fdfa5.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
